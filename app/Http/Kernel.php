1a78a99c352356c1dbad0442007552810f36ab44
ridhopujiono <ridho@smktjp.sch.id>
2022-10-11 14:00:22 +0700

    tambah middleware untuk jwt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'jwt_auth' => \App\Http\Middleware\JwtAuth::class,
     ];
 }
